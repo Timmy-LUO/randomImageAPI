6a85c7510f7f087373f558d032060d7305d1693a
羅承志 <a2200099@yahoo.com>
2021-09-24 13:36:12 +0800

    Initial Commit

---
+++ randomImageAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  randomImageAPI
+//
+//  Created by 羅承志 on 2021/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
