ba155151391bc83ba116f73b6be2037b70dbd1fb
羅承志 <a2200099@yahoo.com>
2021-10-08 17:37:27 +0800

    random image API

--- randomImageAPI/ViewController.swift
+++ randomImageAPI/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        catchImage()
+        TapChange()
     }
-
-
+    
+    @objc func catchImage() {
+        //讀取url
+        let urlStr = "https://picsum.photos/200/300"
+        if let url = URL(string: urlStr) {
+            //於其他執行緒 打開文件，讀取內容
+            URLSession.shared.dataTask(with: url) { (data, response, error) in
+                if let data = data {
+                    //主執行緒執行 顯示內容
+                    DispatchQueue.main.async {
+                        self.imageView.image = UIImage(data: data)
+                    }
+                }
+            }.resume() //執行task
+        }
+    }
+    
+    @IBAction func changImage(_ sender: UIButton) {
+        catchImage()
+    }
+    
+    func TapChange() {
+        //點擊手勢功能，selector為執行function
+        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(catchImage))
+        doubleTap.numberOfTapsRequired = 1 //點擊次數
+        imageView.isUserInteractionEnabled = true //觸碰反應
+        imageView.addGestureRecognizer(doubleTap)  //加入手勢
+    }
 }
 
